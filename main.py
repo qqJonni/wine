e84e6fd8f9d359c754c393fb18a0ecf6304cebdf
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-17 18:46:36 +0400

    Добавил овтоматическое склонение годов

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from datetime import date, datetime
+from datetime import datetime
 
+
+def format_years(number):
+    if number % 100 in [11, 12, 13, 14]:
+        return f"{number} лет"
+
+    last_digit = number % 10
+
+    if last_digit == 1:
+        return f"{number} год"
+    elif last_digit in [2, 3, 4]:
+        return f"{number} года"
+    else:
+        return f"{number} лет"
 
 
 env = Environment(
@@ -15,7 +28,7 @@ current_date = datetime.now()
 time_difference = current_date - past_date
 years_difference = time_difference.days // 365
 rendered_page = template.render(
-    years_old=years_difference,
+    years_old=f'Мы уже {format_years(years_difference)} с вами',
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
