bd8ce92089f7ceda47579dc6ffbc710c6d140b9e
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-19 17:00:41 +0400

    подсушил код

--- main.py
+++ main.py
@@ -47,8 +47,7 @@ if __name__ == '__main__':
 
     template = env.get_template('index.html')
     past_date = datetime(1920, 4, 10)
-    current_date = datetime.now()
-    time_difference = current_date - past_date
+    time_difference = datetime.now() - past_date
     years_difference = time_difference.days // 365
 
     rendered_page = template.render(years_old=f'Мы уже {format_years(years_difference)} с вами', result=result)
