5bd984e5c86f5b6f27ed7ec4c38875367e6e7e42
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-19 16:52:33 +0400

    поменял локигу работы скрипта main.py

--- main.py
+++ main.py
@@ -20,47 +20,42 @@ def format_years(number):
         return f"{number} лет"
 
 
-data = pandas.read_excel('wine.xlsx')
-
-result = defaultdict(list)
-
-for _, row in data.iterrows():
-    category = row['Категория']
-    product = {
-        'Название': row['Название'],
-        'Сорт': row['Сорт'],
-        'Цена': row['Цена'],
-        'Картинка': row['Картинка']
-    }
-    result[category].append(product)
-
-result = dict(result)
-
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('index.html')
-past_date = datetime(1920, 4, 10)
-current_date = datetime.now()
-time_difference = current_date - past_date
-years_difference = time_difference.days // 365
-
-
-rendered_page = template.render(years_old=f'Мы уже {format_years(years_difference)} с вами', result=result)
-
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate website from wine Excel data.')
     parser.add_argument('filename', help='The Excel filename')
     args = parser.parse_args()
 
     data = pandas.read_excel(args.filename)
-# python main.py wine.xlsx
+    result = defaultdict(list)
+
+    for _, row in data.iterrows():
+        category = row['Категория']
+        product = {
+            'Название': row['Название'],
+            'Сорт': row['Сорт'],
+            'Цена': row['Цена'],
+            'Картинка': row['Картинка']
+        }
+        result[category].append(product)
+
+    result = dict(result)
+
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('index.html')
+    past_date = datetime(1920, 4, 10)
+    current_date = datetime.now()
+    time_difference = current_date - past_date
+    years_difference = time_difference.days // 365
+
+    rendered_page = template.render(years_old=f'Мы уже {format_years(years_difference)} с вами', result=result)
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
